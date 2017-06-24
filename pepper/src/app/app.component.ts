acb7c77026412b60ac752ba3a644c81bcb069acc
SergiOn <Onishhenko@gmail.com>
2017-06-25 01:05:35 +0300

    section #1 #lecture-5 # add pepper project

--- pepper/src/app/app.component.ts
+++ pepper/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  // items: FirebaseListObservable<any[]>;
+
+  constructor(fb: AngularFireDatabase) {
+    console.log(fb);
+  }
 }
--- pepper/src/app/app.module.ts
+++ pepper/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDZN92wW7u2Mn9sep1S_qI_WAwmkpsd93o',
+  authDomain: 'pepper-90f68.firebaseapp.com',
+  databaseURL: 'https://pepper-90f68.firebaseio.com',
+  projectId: 'pepper-90f68',
+  storageBucket: 'pepper-90f68.appspot.com',
+  messagingSenderId: '686885962278'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
