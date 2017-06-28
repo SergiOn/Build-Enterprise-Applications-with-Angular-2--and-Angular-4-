dfb4e21c8588c8e0968c353621ae23f87dd0bc39
SergiOn <Onishhenko@gmail.com>
2017-06-28 22:39:34 +0300

    section #2 #lecture-15 # restaurants

--- pepper/src/app/app.component.ts
+++ pepper/src/app/app.component.ts
@@ -7,9 +7,8 @@ import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} f
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
   cuisines: FirebaseListObservable<any[]>;
-  // restaurant: FirebaseObjectObservable<any[]>;
+  restaurants: FirebaseObjectObservable<any[]>;
 
   constructor(
     private af: AngularFireDatabase
@@ -17,27 +16,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.cuisines = this.af.list('/cuisines');
-    // this.restaurant = this.af.object('/restaurant');
-  }
-
-  add() {
-    // this.cuisines.push({
-    //   4: 'Asian'
-    // });
-    this.cuisines.push('Asian');
-  }
-
-  update() {
-    this.af.object('/restaurant').update({
-      name: 'New Name',
-      rating: 5
-    });
-    this.af.object('/favorites/1/10').set(null);
-  }
-
-  remove() {
-    this.af.object('/restaurant').remove()
-      .then((x) => console.log('SUCCESS', x))
-      .catch((er) => console.log('error', er));
+    this.restaurants = this.af.object('/restaurants');
   }
 }
