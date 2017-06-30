999395f9777c6435a2c7cee4aa557c35c3219dc5
SergiOn <onishhenko@gmail.com>
2017-06-30 13:40:49 +0300

    section #2 #lecture-18 # sorting

--- pepper/src/app/app.component.ts
+++ pepper/src/app/app.component.ts
@@ -11,39 +11,46 @@ import 'rxjs/add/operator/take';
 })
 export class AppComponent implements OnInit {
   cuisines: FirebaseListObservable<any[]>;
-  // restaurants: FirebaseListObservable<any[]>;
-  restaurants: Observable<any[]>;
-  exists;
+  restaurants: FirebaseListObservable<any[]>;
+  // restaurants: Observable<any[]>;
+  // exists;
 
   constructor(
     private af: AngularFireDatabase
   ) { }
 
   ngOnInit() {
-    this.cuisines = this.af.list('/cuisines');
-    this.restaurants = this.af.list('/restaurants')
-      .map((restaurants) => {
-        restaurants.map((restaurant) => {
-          restaurant.cuisineType = this.af.object(`/cuisines/${restaurant.cuisine}`);
-
-          restaurant.featureTypes = [];
-          for (const f of Object.keys(restaurant.features)) {
-            restaurant.featureTypes.push(this.af.object(`/features/${f}`));
-          }
-        });
-        return restaurants;
-      });
-
-    this.exists = this.af.object('/restaurants/0/features/0');
-
-    this.exists.take(1).subscribe((x) => {
-      console.log(x);
-
-      if (x && x.$value) {
-        console.log('EXISTS');
-      } else {
-        console.log('NOT EXISTS');
+    this.cuisines = this.af.list('/cuisines', {
+      query: {
+        orderByValue: true
       }
     });
+
+    this.restaurants = this.af.list('/restaurants');
+
+    // this.restaurants = this.af.list('/restaurants')
+    //   .map((restaurants) => {
+    //     restaurants.map((restaurant) => {
+    //       restaurant.cuisineType = this.af.object(`/cuisines/${restaurant.cuisine}`);
+    //
+    //       restaurant.featureTypes = [];
+    //       for (const f of Object.keys(restaurant.features)) {
+    //         restaurant.featureTypes.push(this.af.object(`/features/${f}`));
+    //       }
+    //     });
+    //     return restaurants;
+    //   });
+    //
+    // this.exists = this.af.object('/restaurants/0/features/0');
+    //
+    // this.exists.take(1).subscribe((x) => {
+    //   console.log(x);
+    //
+    //   if (x && x.$value) {
+    //     console.log('EXISTS');
+    //   } else {
+    //     console.log('NOT EXISTS');
+    //   }
+    // });
   }
 }
