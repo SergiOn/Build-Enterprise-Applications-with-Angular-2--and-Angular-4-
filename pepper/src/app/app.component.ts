400c7b967b8048ed8b0ad193d22f3bac4f49456f
SergiOn <Onishhenko@gmail.com>
2017-07-01 22:39:01 +0300

    section #3 #lecture-25 # Implementing Facebook Login

--- pepper/src/app/app.component.ts
+++ pepper/src/app/app.component.ts
@@ -1,84 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
-import { Observable } from 'rxjs/observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/take';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AngularFireAuth]
 })
 export class AppComponent implements OnInit {
-  cuisines: FirebaseListObservable<any[]>;
-  restaurants: FirebaseListObservable<any[]>;
-  // restaurants: Observable<any[]>;
-  // exists;
+  user: Observable<firebase.User>;
 
   constructor(
-    private af: AngularFireDatabase
-  ) { }
-
-  ngOnInit() {
-    this.af.list('/restaurants').push({name: ''})
-      .then((x) => {
-        console.log(x);
-        console.log(x.key);
-        const restaurant = { name: 'My new restaurant' };
-
-        const update = {
-          [`restaurants/${x.key}`]: restaurant,
-          [`restaurants-by-city/camberwell/${x.key}`]: restaurant
-          // [`restaurants-by-city/camberwell/${x.key}`]: null  // remove
-        };
-
-        this.af.object('/').update(update);
-      });
-
-
-    /*
-    this.cuisines = this.af.list('/cuisines', {
-      query: {
-        orderByValue: true,
-        equalTo: 'Italian'
-      }
-    });
-
-    this.restaurants = this.af.list('/restaurants', {
-      query: {
-        orderByChild: 'rating',
-        // startAt: 3,
-        // endAt: 4
-        equalTo: 5,
-        // limitToFirst: 50
-        limitToLast: 2
-      }
-    });
-    */
-
-    // this.restaurants = this.af.list('/restaurants')
-    //   .map((restaurants) => {
-    //     restaurants.map((restaurant) => {
-    //       restaurant.cuisineType = this.af.object(`/cuisines/${restaurant.cuisine}`);
-    //
-    //       restaurant.featureTypes = [];
-    //       for (const f of Object.keys(restaurant.features)) {
-    //         restaurant.featureTypes.push(this.af.object(`/features/${f}`));
-    //       }
-    //     });
-    //     return restaurants;
-    //   });
-    //
-    // this.exists = this.af.object('/restaurants/0/features/0');
-    //
-    // this.exists.take(1).subscribe((x) => {
-    //   console.log(x);
-    //
-    //   if (x && x.$value) {
-    //     console.log('EXISTS');
-    //   } else {
-    //     console.log('NOT EXISTS');
-    //   }
-    // });
+    public afAuth: AngularFireAuth
+  ) {
+    this.user = afAuth.authState;
   }
+
+  ngOnInit() { }
+
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+      .then(authState => {
+        console.log('authState', authState);
+      })
+      .catch(error => {
+        console.log('error', error);
+      });
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
 }
