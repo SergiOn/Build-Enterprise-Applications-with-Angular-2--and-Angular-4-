1679a0403794929fae65a32087eb60b7f3379ce5
SergiOn <Onishhenko@gmail.com>
2017-07-08 23:05:27 +0300

    section #3 #lecture-28 # Calling Facebook Graph API

--- pepper/src/app/app.component.ts
+++ pepper/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database';
@@ -16,6 +17,7 @@ export class AppComponent implements OnInit {
   photoURL;
 
   constructor(
+    public http: Http,
     public afAuth: AngularFireAuth,
     public af: AngularFireDatabase
   ) {
@@ -27,6 +29,22 @@ export class AppComponent implements OnInit {
         this.photoURL = null;
         return;
       }
+
+      console.log(authState);
+
+      const userRef = af.object(`/users/${authState.uid}`);
+      userRef.subscribe(user => {
+        const url = `https://graph.facebook.com/v2.9/${authState.providerData[0].uid}?fields=first_name,last_name,gender&access_token=${user.accessToken}`;
+
+        this.http.get(url).subscribe(response => {
+          const user = response.json();
+          userRef.update({
+            firstName: user.first_name,
+            lastName: user.last_name
+          });
+          console.log(user);
+        });
+      });
 
       this.displayName = authState.displayName;
       this.photoURL = authState.photoURL;
