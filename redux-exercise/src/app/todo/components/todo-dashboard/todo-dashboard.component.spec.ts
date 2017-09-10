b37a79189a4d64a3d6a468727d48736c2193f89f
SergiOn <Onishhenko@gmail.com>
2017-09-10 17:42:13 +0300

    section #9 #lecture-103 # Exercise # rename dashboard component

--- redux-exercise/src/app/todo/components/todo-dashboard/todo-dashboard.component.spec.ts
+++ redux-exercise/src/app/todo/components/todo-dashboard/todo-dashboard.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DashboardComponent } from './dashboard.component';
+import { TodoDashboardComponent } from './todo-dashboard.component';
 
-describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+describe('TodoDashboardComponent', () => {
+  let component: TodoDashboardComponent;
+  let fixture: ComponentFixture<TodoDashboardComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ TodoDashboardComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
+    fixture = TestBed.createComponent(TodoDashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
