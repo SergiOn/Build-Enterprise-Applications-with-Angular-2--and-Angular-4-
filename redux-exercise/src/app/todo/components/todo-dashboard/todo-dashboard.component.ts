92fd12248d1fe291b16eae286579cdedf44218f2
SergiOn <Onishhenko@gmail.com>
2017-09-11 20:33:25 +0300

    section #9 #lecture-103 # Exercise # complete todo-dashboard component

--- redux-exercise/src/app/todo/components/todo-dashboard/todo-dashboard.component.ts
+++ redux-exercise/src/app/todo/components/todo-dashboard/todo-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { select } from 'ng2-redux';
+
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-dashboard',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoDashboardComponent implements OnInit {
 
-  constructor() { }
+  @select(['todo', 'lastUpdate']) lastUpdate;
+  @select(t => t.todo.todos.toJS()) todos;
+  @select(t => t.todo.todos.toJS().length) todoLength;
+
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
+  }
+
+  deleteTodos() {
+    let arrTodos = [];
+    this.todos.subscribe(t => arrTodos = t || []);
+    this.todoService.deleteTodos(arrTodos);
   }
 
 }
