8936a0bfaca1998800fc3e76d59e9245bf6937f6
SergiOn <Onishhenko@gmail.com>
2017-09-11 20:33:42 +0300

    section #9 #lecture-103 # Exercise # complete actions

--- redux-exercise/src/app/store/actions/todo.actions.ts
+++ redux-exercise/src/app/store/actions/todo.actions.ts
@@ -1,4 +1,5 @@
 export const FETCH_TODOS = 'FETCH_TODOS';
 export const ADD_TODO = 'ADD_TODO';
-export const UPDATE_TODO = 'UPDATE_TODO';
-export const DELETE_TODO = 'DELETE_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const DELETE_TODOS = 'DELETE_TODOS';
