ebbbffd7b889dbb66f0a67524235f364206331d0
SergiOn <Onishhenko@gmail.com>
2017-09-12 16:28:30 +0300

    section #10 #lecture-113 # Set Up and Tear Down

--- testing-demo/src/app/03-setup-and-teardown/vote.component.spec.ts
+++ testing-demo/src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,22 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  it('', () => {
+  it('should increment totalVotes when upvoted', () => {
+    // Arrange
+    const component = new VoteComponent();
+
+    // Act
+    component.upVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(1);
   });
 
-  it('', () => {
+  it('should decrement totalVotes when downvoted', () => {
+    const component = new VoteComponent();
+    component.downVote();
+
+    expect(component.totalVotes).toBe(-1);
   });
+
 });
