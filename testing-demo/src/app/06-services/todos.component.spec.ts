5922c087b6b648ce7344172daadf34d65d709787
SergiOn <Onishhenko@gmail.com>
2017-09-13 13:55:36 +0300

    section #10 #lecture-117 # Interaction Testing

--- testing-demo/src/app/06-services/todos.component.spec.ts
+++ testing-demo/src/app/06-services/todos.component.spec.ts
@@ -2,6 +2,8 @@ import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/throw';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -19,5 +21,28 @@ describe('TodosComponent', () => {
     expect(component.todos.length).toBeGreaterThan(0);
     expect(component.todos.length).toBe(3);
     expect(component.todos).toEqual([1, 2, 3]);
+  });
+
+  it('should call the server to save the changes when a new todo item is added', () => {
+    const spy = spyOn(service, 'addTodo').and.callFake((t) => Observable.empty());
+    component.addTodo();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should add new todo returned from the server', () => {
+    const todo = {id: 1};
+    const spy = spyOn(service, 'addTodo').and.returnValue(Observable.from([ todo ]));
+    component.addTodo();
+
+    expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+  });
+
+  it('should set the message property if server returns an error when adding a new todo', () => {
+    const error = 'error from the server';
+    const spy = spyOn(service, 'addTodo').and.returnValue(Observable.throw(error));
+    component.addTodo();
+
+    expect(component.message).toBe(error);
   });
 });
