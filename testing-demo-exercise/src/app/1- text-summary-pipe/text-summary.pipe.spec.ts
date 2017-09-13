3efb3d0ca7243b223612ac51f4c3a2587cf20f79
SergiOn <Onishhenko@gmail.com>
2017-09-13 15:41:25 +0300

    section #10 #lecture-121 # Exercises # 1-text-summary-pipe

--- testing-demo-exercise/src/app/1- text-summary-pipe/text-summary.pipe.spec.ts
+++ testing-demo-exercise/src/app/1- text-summary-pipe/text-summary.pipe.spec.ts
@@ -1,18 +1,33 @@
 import { TextSummaryPipe } from './text-summary.pipe';
 
 describe('TextSummaryPipe', () => {
+  let sut: TextSummaryPipe;
 
-  beforeEach(() => {});
+  beforeEach(() => {
+    sut = new TextSummaryPipe();
+  });
 
-  it('should return an empty string if input is null', () => {});
+  it('should return an empty string if input is null', () => {
+    expect(sut.transform(null)).toBe('');
+  });
 
-  it('should return null if input is undefined', () => {});
+  it('should return null if input is undefined', () => {
+    expect(sut.transform(undefined)).toBe('');
+  });
 
-  it('should return empty string if input is an empty string', () => {});
+  it('should return empty string if input is an empty string', () => {
+    expect(sut.transform('')).toBe('');
+  });
 
-  it('should return the same string if the length of input is less than the limit', () => {});
+  it('should return the same string if the length of input is less than the limit', () => {
+    expect(sut.transform('12345', 5)).toBe('12345');
+  });
 
-  it('should summarize the input if it is longer than the limit', () => {});
+  it('should summarize the input if it is longer than the limit', () => {
+    expect(sut.transform('12345678', 5)).toBe('12345...');
+  });
 
-  it('should assume 10 as the limit if not given', () => {});
+  it('should assume 10 as the limit if not given', () => {
+    expect(sut.transform('12345678901')).toBe('1234567890...');
+  });
 });
