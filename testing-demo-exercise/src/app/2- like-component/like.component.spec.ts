da4cb8010a8cb9bd1219ee1dbc20fe93cd2e6056
SergiOn <Onishhenko@gmail.com>
2017-09-13 15:49:13 +0300

    section #10 #lecture-121 # Exercises # 2-like-component

--- testing-demo-exercise/src/app/2- like-component/like.component.spec.ts
+++ testing-demo-exercise/src/app/2- like-component/like.component.spec.ts
@@ -1,12 +1,26 @@
 import { LikeComponent } from './like.component';
 
 describe('LikeComponent', () => {
+    let sut: LikeComponent;
 
-    beforeEach(() => {});
+    beforeEach(() => {
+      sut = new LikeComponent();
+    });
 
-    it('should toggle the iLike property when I click it', () => {});
+    it('should toggle the iLike property when I click it', () => {
+      sut.click();
+      expect(sut.iLike).toBe(true);
+    });
 
-    it('should decrement total likes if I like an object and click the LikeComponent to unlike it', () => {});
+    it('should decrement total likes if I like an object and click the LikeComponent to unlike it', () => {
+      sut.iLike = true;
+      sut.click();
 
-    it('should increment total likes if I do NOT like an object and click the LikeComponent to like it', () => {});
+      expect(sut.totalLikes).toBe(-1);
+    });
+
+    it('should increment total likes if I do NOT like an object and click the LikeComponent to like it', () => {
+      sut.click();
+      expect(sut.totalLikes).toBe(1);
+    });
 });
