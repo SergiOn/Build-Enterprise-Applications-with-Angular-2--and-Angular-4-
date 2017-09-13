4b42421e040697b72b8af30405c8b22fa877fcf3
SergiOn <Onishhenko@gmail.com>
2017-09-14 01:54:52 +0300

    section #11 #lecture-125 # Testing Property Bindings

--- integration-testing-demo/src/app/1-voter/voter.component.spec.ts
+++ integration-testing-demo/src/app/1-voter/voter.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { VoterComponent } from './voter.component';
 
 describe('VoterComponent', () => {
@@ -16,6 +17,28 @@ describe('VoterComponent', () => {
     // fixture.debugElement;
   });
 
-  it('', () => {
+  it('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    const de = fixture.debugElement.query(By.css('.vote-count'));
+    // fixture.debugElement.queryAll(By.css('.vote-count'));
+    // fixture.debugElement.query(By.directive(VoterComponent));
+    const el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain('21');
+    // expect(el.innerText).toBe('21');
   });
+
+  it('should highlight the upvote button if I have upvoted', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    const de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+
+    // de.attributes
+    expect(de.classes['highlighted']).toBe(true);
+  });
+
 });
